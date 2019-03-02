ebce6b0162e57977b78073b5dfd3ca8429ed3a19
sunqiang <sqqsun@gmail.com>
2019-03-02 15:05:14 +0800

    no message

--- src/swoorpc/config.php
+++ src/swoorpc/config.php
@@ -21,6 +21,7 @@ return [
             'socket_buffer_size' => 1024 * 1024 * 2,
             'task_worker_num' => 100, //task进程的数量
             'task_max_request' => 100, //task进程的最大任务数
+            'open_tcp_nodelay' => true,
         ]
     ],
 
